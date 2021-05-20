cdec5633d03ab47ef6fb2878a7b2f16b27dcb450
mmorrisonlk <mmorrisonlk@gmail.com>
2021-05-19 22:51:31 -0500

    Added time to now

--- utils/helpers.js
+++
@@ -1,8 +0,0 @@
-const formatDistance = require('date-fns/formatDistance');
-const subDays = require('date-fns/subDays');
-
-module.exports = {
-    format_date: (date) => {
-        formatDistance(subDays(new Date(), 3), new Date(date), { addSuffix: true })
-    }
-}
