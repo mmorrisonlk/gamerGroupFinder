63043326f2af983f1cb7713607ce79938ab61431
unknown <ashleyaggie@aol.com>
2021-05-19 23:41:12 -0500

    Added div for time since Group was posted.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,17 @@
-const formatDistance = require('date-fns/formatDistance');
-const subDays = require('date-fns/subDays');
+const formatDistanceToNow = require('date-fns/formatDistanceToNow');
 
 module.exports = {
     format_date: (date) => {
-        formatDistance(subDays(new Date(), 3), new Date(date), { addSuffix: true })
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString();
     },
     if_eq: (arg1, arg2) => {
         console.log(arg1, arg2);
         return arg1 === arg2
+    },
+    formatTime: function(date) 
+    {
+        return formatDistanceToNow(new Date(date), { addSuffix: true })
     }
     
 }
